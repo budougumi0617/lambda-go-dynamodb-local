3b37f8ae5bba843d47c4ce1d083857d6532b246f
budougumi0617 <budougumi0617@gmail.com>
2019-06-02 13:16:36 +0900

    :white_check_mark: Add test

--- user/main.go
+++ user/main.go
@@ -19,58 +19,65 @@ type User struct {
 	Name string `json:"name"`
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// Environment variables
-	endpoint := os.Getenv("DYNAMODB_ENDPOINT")
-	tableName := os.Getenv("DYNAMODB_TABLE_NAME")
+func generateHandler(endpoint, region, tableName string) func(events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		// Request
+		id, _ := request.PathParameters["id"]
 
-	// Request
-	id, _ := request.PathParameters["id"]
+		// DynamoDB
+		sess := session.Must(session.NewSession())
+		config := aws.NewConfig().WithRegion(region)
+		if len(endpoint) > 0 {
+			config = config.WithEndpoint(endpoint)
+		}
 
-	// DynamoDB
-	sess := session.Must(session.NewSession())
-	config := aws.NewConfig().WithRegion("ap-northeast-1")
-	if len(endpoint) > 0 {
-		config = config.WithEndpoint(endpoint)
-	}
-
-	db := dynamodb.New(sess, config)
-	response, err := db.GetItem(&dynamodb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"Id": {
-				N: aws.String(string(id)),
+		db := dynamodb.New(sess, config)
+		response, err := db.GetItem(&dynamodb.GetItemInput{
+			TableName: aws.String(tableName),
+			Key: map[string]*dynamodb.AttributeValue{
+				"Id": {
+					N: aws.String(string(id)),
+				},
 			},
-		},
-		AttributesToGet: []*string{
-			aws.String("Id"),
-			aws.String("Name"),
-		},
-		ConsistentRead:         aws.Bool(true),
-		ReturnConsumedCapacity: aws.String("NONE"),
-	})
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
+			AttributesToGet: []*string{
+				aws.String("Id"),
+				aws.String("Name"),
+			},
+			ConsistentRead:         aws.Bool(true),
+			ReturnConsumedCapacity: aws.String("NONE"),
+		})
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
 
-	user := User{}
-	err = dynamodbattribute.Unmarshal(&dynamodb.AttributeValue{M: response.Item}, &user)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
+		user := User{}
+		err = dynamodbattribute.Unmarshal(&dynamodb.AttributeValue{M: response.Item}, &user)
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
 
-	// Json
-	bytes, err := json.Marshal(user)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
+		// Json
+		bytes, err := json.Marshal(user)
+		if err != nil {
+			return events.APIGatewayProxyResponse{}, err
+		}
 
-	return events.APIGatewayProxyResponse{
-		Body:       string(bytes),
-		StatusCode: http.StatusOK,
-	}, nil
+		return events.APIGatewayProxyResponse{
+			Body:       string(bytes),
+			StatusCode: http.StatusOK,
+		}, nil
+	}
 }
 
 func main() {
-	lambda.Start(handler)
+	// Environment variables
+	endpoint := os.Getenv("DYNAMODB_ENDPOINT")
+	region := os.Getenv("AWS_REGION")
+	if len(region) == 0 {
+		region = "ap-northeast-1"
+	}
+	tableName := os.Getenv("DYNAMODB_TABLE_NAME")
+
+	h := generateHandler(endpoint, region, tableName)
+	lambda.Start(h)
 }
